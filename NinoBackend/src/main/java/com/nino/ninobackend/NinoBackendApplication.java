b280c8edbeb906359b50d2e9b0b734b76bf17384
Nino <840654613@qq.com>
2023-10-30 13:28:08 +0800

    init lombok & Spring Web

---
+++ NinoBackend/src/main/java/com/nino/ninobackend/NinoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nino.ninobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NinoBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NinoBackendApplication.class, args);
+    }
+
+}
